90707b76c4cc22e50b5b2658f955ebebfaab1de0
Nitant Joshi <nitantjoshi20@gmail.com>
2021-01-19 22:22:58 +0530

    User API is added with CRUD fucntionality without ORM

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,8 +1,152 @@
 const express = require('express');
 const router = express.Router();
+const { pool } = require('../../app');
 
+//Get all users
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: "Get ALl users"
+    pool.connect().then((client) => {
+        const query = 'Select * from public."Users"';
+        client.query(query).then(result => {
+            const resp = {
+                count: result.rowCount,
+                users: result.rows,
+                request: {
+                    type: "GET",
+                    url: "http://localhost:3000/users/userId"
+                }
+            };
+            res.status(200).json(resp);
+        }).catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        })
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
+});
+
+//Get specific User
+router.get('/:userId', (req, res, next) => {
+    const id = req.params.userId;
+    pool.connect().then(client => {
+        const query = 'Select * from public."Users" where "userId" = $1';
+        client.query(query, [id]).then(result => {
+            res.status(200).json({
+                user: result.rows[0],
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:3000/users'
+                }
+            });
+        }).catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        })
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
+});
+
+//Create new user
+router.post('/', (req, res, next) => {
+    const userName = req.body.userName;
+    const userContactNumber = req.body.userContactNumber;
+    const userCity = req.body.userCity;
+    const userState = req.body.userState;
+
+    pool.connect().then(client => {
+        const query = 'Insert into public."Users"("userName","userContactNumber","userCity","userState") Values($1,$2,$3,$4)';
+        client.query(query, [userName, userContactNumber, userCity, userState]).then(result => {
+            res.status(201).json({
+                message: "Created user successfully",
+                request: {
+                    type: 'GET',
+                    url: "http://localhost:3000/users/"
+                },
+            })
+        }).catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        })
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
+});
+
+//Update user
+router.patch('/:userId', (req, res, next) => {
+    const userId = req.params.userId;
+    const userName = req.body.userName;
+    const userContactNumber = req.body.userContactNumber;
+    const userCity = req.body.userCity;
+    const userState = req.body.userState;
+
+    pool.connect().then(client => {
+        const query = 'Update public."Users" SET "userName" = $1,"userContactNumber"=$2,"userCity"=$3,"userState"=$4 where "userId"=$5';
+        client.query(query, [userName, userContactNumber, userCity, userState, userId]).then(result => {
+            res.status(201).json({
+                message: "User Updated",
+                request: {
+                    type: 'GET',
+                    url: `http://localhost:3000/users/${userId}`
+                },
+            })
+        }).catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        })
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
+});
+
+//Delete exisitng user
+router.delete('/:userId', (req, res, next) => {
+    const id = req.params.userId;
+
+    pool.connect().then(client => {
+        const query = 'Delete from public."Users" where "userId" = $1';
+        client.query(query, [id]).then(result => {
+            res.status(200).json({
+                message: 'User deleted',
+                request: {
+                    type: 'POST',
+                    url: 'http://localhost:3000/users',
+                }
+            });
+
+        }).catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            })
+        })
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err,
+        })
     })
 });
+
+module.exports = router;
--- app.js
+++ app.js
@@ -1,8 +1,59 @@
+//External Packages
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const bodyParser = require("body-parser");
+const { Pool } = require('pg');
 
+//Creating database pool
+const pool = new Pool({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT,
+})
+exports.pool = pool;
+
+//Get the routes
 const userRoutes = require('./api/routes/users');
 
+//Parsing the body
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Allowing Cors 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
+//Routes which should handle the request
 app.use('/users', userRoutes);
 
-module.exports = app;
+//If nothing is matched
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+});
+
+//Handling errors
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+})
+exports.app = app;
